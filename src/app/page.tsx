9c003d151b8ed4f2c625aab6bd94cb75fc44b242
JUSTMEETPATEL <justmeetpatel@gmail.com>
2024-10-20 11:51:08 +0530

    Project section changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,12 @@ import HomeComponent from "@/components/Home";
 import Navbar from "@/components/Navbar";
 // import Projects from "@/components/Projects";
 import Service from "@/components/Service";
-import { LayoutGrid } from "@/components/ui/layout-grid";
+import { FocusCards } from "@/components/ui/focus-cards";
 import { Separator } from "@/components/ui/separator";
 import { cards } from "@/data/constants";
 
 export default function Home() {
+  
   return (
     <>
       <section id="nav">
@@ -28,13 +29,13 @@ export default function Home() {
         <Separator className="my-10" />
         <Clients />
       </section> */}
-      <section id="projects" className="h-screen py-20 w-full">
+      <section id="projects">
         <Separator className="my-20" />
         {/* <Projects /> */}
         <div>
           <h2 className="text-5xl font-bold mb-24 text-center">Projects</h2>
         </div>
-        <LayoutGrid cards={cards} />
+        <FocusCards cards={cards}/>
       </section>
       <section className="pt-32">
         <Separator className="my-20" />
--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,31 +1,47 @@
 export const cards = [
     {
-      id: 1,
-      content: "",
-      className: "md:col-span-2",
-      thumbnail:
-        "/1.png",
+      title: "",
+      src: "/1.png",
     },
     {
-      id: 2,
-      content: "",
-      className: "col-span-1",
-      thumbnail:
-        "/2.png",
+      title: "",
+      src: "/2.png",
     },
     {
-      id: 3,
-      content: "",
-      className: "col-span-1",
-      thumbnail:
-        "/3.png",
+      title: "",
+      src: "/3.png",
     },
     {
-      id: 4,
-      content: "",
-      className: "md:col-span-2",
-      thumbnail:
-        "/4.png",
+      title: "",
+      src: "/4.png",
     },
+    {
+      title: "",
+      src: "/5.png",
+    },
+    {
+      title: "",
+      src: "/6.png",
+    },
+    // {
+    //   title: "",
+    //   src: "/7.png",
+    // },
+    // {
+    //   title: "",
+    //   src: "/8.png",
+    // },
+    // {
+    //   title: "",
+    //   src: "/9.png",
+    // },
+    // {
+    //   title: "",
+    //   src: "/10.png",
+    // },
+    // {
+    //   title: "",
+    //   src: "/11.png",
+    // },
   ];
 
